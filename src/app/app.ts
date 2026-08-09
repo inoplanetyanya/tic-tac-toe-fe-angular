fa4f3588228fc9553546c68f398b0b1d665bcc69
inoplanetyanya <inoplanetyanya97@gmail.com>
2026-08-09 08:04:30 +0300

    add app-button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppInput } from './shared/ui/app-input/app-input';
+import { AppButton } from './shared/ui/app-button/app-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppInput],
+  imports: [RouterOutlet, AppInput, AppButton],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular');
+
+  protected onButtonClick(): void {
+    console.log('asdf')
+  }
 }
