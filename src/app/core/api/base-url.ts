a6a925288e6c39ed0d65352fc033e0e876d7caa6
inoplanetyanya <inoplanetyanya97@gmail.com>
2026-08-27 03:31:35 +0300

    feat: websocket

--- src/app/core/api/base-url.ts
+++ src/app/core/api/base-url.ts
@@ -1,3 +1,4 @@
 import { environment } from "../../../environments/environment";
 
 export const API_BASE_URL = environment.apiBaseUrl;
+export const WEB_SOCKET_URL = environment.webSocketUrl;
---
+++ src/app/core/api/game/game-ws.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { WEB_SOCKET_URL } from '~api/base-url';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GameWsService {
+  private readonly authService = inject(AuthService);
+
+  private socket: WebSocket | null = null;
+
+  public readonly isConnected = signal<boolean>(false);
+
+  public connect(): void {
+    if (this.socket) return;
+
+    this.socket = new WebSocket(WEB_SOCKET_URL);
+
+    this.socket.onopen = () => {
+      this.isConnected.set(true);
+      const token = this.authService.accessToken();
+      this.sendMessage(`/auth ${token}`);
+    };
+
+    this.socket.onmessage = (event) => {
+      this.handleServerMessage(event.data);
+    };
+
+    this.socket.onclose = () => {
+      this.isConnected.set(false);
+      this.socket = null;
+    };
+  }
+
+  public sendMessage(msg: string): void {
+    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+      this.socket.send(msg);
+    }
+  }
+
+  private handleServerMessage(message: string): void {
+    console.log('[WS Получено]:', message);
+  }
+
+  public disconnect(): void {
+    if (this.socket) {
+      this.sendMessage('/disconnect');
+      this.socket.close();
+    }
+  }
+}
